68d6ab5234b2d2e6d1077157a372ae3950eb9876
Madison Pratt <madisonpr94@gmail.com>
2022-01-27 22:49:05 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Eve.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
